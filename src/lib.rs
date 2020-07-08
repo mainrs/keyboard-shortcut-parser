4654b50d53d743625fac1c52d22db7420cb4fd9d
Sven Lechner <SirWindfield@users.noreply.github.com>
2020-07-08 15:41:14 +0200

    docs: add missing documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! This crate allows to parse keyboard shortcut strings and maps the results to the right keycodes.
+//! Keycode mapping is currently supported for X11 and Windows.
+
+#[deny(missing_docs, missing_copy_implementations, missing_debug_implementations)]
+
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
@@ -7,10 +12,14 @@ pub use parser::parse_key_string;
 mod platform;
 pub use platform::{KeyModifier, KeySpecial};
 
+/// A keyboard key.
 #[derive(Copy, Clone, Debug, Eq, Hash, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
 pub enum Key {
+    /// An alphanumeric key ([a-zA-Z0-9]).
     Alphanumeric(char),
+    /// A modifier key (Alt, Control, Shift or Super).
     Modifier(KeyModifier),
+    /// A special key (anything that is not one of the above).
     Special(KeySpecial),
 }
